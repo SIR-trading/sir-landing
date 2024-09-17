3d6981ec5c0dc12f9795a6719a3f0f8f96d6efa6
abstrucked <mxhgraphics@gmail.com>
2024-09-17 13:00:33 +0200

    Update dependencies in yarn.lock
    Upgraded several packages. This includes improvements in performance, security, and compatibility adjustments.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,22 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt"],
+
   colorMode: {
     preference: 'dark',
   },
+
   tailwindcss: {
     cssPath: '~/assets/global.css',
   },
+
   runtimeConfig: {
     public: {
       testnetContract: process.env.NUXT_TESTNET_CONTRACT,
       contract: process.env.NUXT_CONTRACT,
       env: process.env.NUXT_NODE_ENV,
     }
-  }
+  },
+
+  compatibilityDate: "2024-09-16"
 })
