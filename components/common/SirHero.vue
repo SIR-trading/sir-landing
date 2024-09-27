fa242e6118918d6d7ed3662b6c43412f8ba19076
abstrucked <mxhgraphics@gmail.com>
2024-09-27 18:08:43 +0200

    Enhance SirHero component styles and update hero image
    Applied additional styling to the <picture> element in SirHero component to improve responsiveness and spacing. Updated the hero image in the index page to reflect a new borderless version.

--- components/common/SirHero.vue
+++ components/common/SirHero.vue
@@ -7,11 +7,10 @@
 <template>
   <div>
     <div class="flex flex-col items-center justify-center p-0">
-      <picture v-if="image">
+      <picture v-if="image" class="p-0 w-full md:w-[760px]">
         <img :src="image" alt="Hero Image">
       </picture>
-
-        <h1 class="text-4xl title text-center">
+        <h1 class="text-4xl title text-center mt-3">
           <slot name="title"/>
         </h1>
     </div>
