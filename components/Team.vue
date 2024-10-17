0d17df764aff6ede6ff7016d2c81b71741e5bd45
abstrucked <mxhgraphics@gmail.com>
2024-10-17 15:01:05 +0200

    Update team member avatars and add social media links
    Added new avatar images for team members and updated their profiles to include social media links. Enhanced the UI for team and thanks sections to display avatars and aligned styles.

--- components/Team.vue
+++ components/Team.vue
@@ -1,18 +1,23 @@
 <script lang="ts" setup>
 
 const teamMembers = [
-  {name: "@Xatarrer", role: "Founder  / Smart contract dev", link: "", avatar: ""},
-  {name: "@realtarpcity", role: "Fronted dev", link: "", avatar: ""},
-  {name: "@Abstrucked", role: "Fronted dev", link: "abstrucked", avatar: ""},
-  {name: "@MrLivingstream", role: "UX/UI designer", link: "", avatar: ""},
-  {name: "@Crashnaut", role: "Community manager", link: "", avatar: ""},
-  {name: "@0xjohn", role: "Frontend dev", link: "", avatar: ""},
-  {name: "@Pau", role: "Ambassador", link: "", avatar: ""},
+  {
+    name: "Xatarrer",
+    role: "Founder  / Smart contract dev",
+    link: "https://x.com/Xatarrer",
+    avatar: "team-avatars/xatarrer.jpg"
+  },
+  {name: "Realtarpcity", role: "Fronted dev", link: "https://x.com/tarpley_travis", avatar: "team-avatars/tarp.jpg"},
+  {name: "Abstrucked", role: "Fronted dev", link: "https://x.com/abstrucked", avatar: "team-avatars/abstrucked.jpg"},
+  {name: "MrLivingstream", role: "UX/UI designer", link: "https://x.com/MrLivingstream", avatar: "team-avatars/MrLivingstream.jpg"},
+  {name: "Crashnaut", role: "Community manager", link: "https://x.com/crashnaut", avatar: "team-avatars/crashnaut.jpg"},
+  {name: "0xjohn", role: "Frontend dev", link: "https://x.com/0xjooohn", avatar: "team-avatars/0xjohn.jpg"},
+  {name: "Pau", role: "Ambassador", link: "https://x.com/PauChan23", avatar: "team-avatars/pau.jpg"},
 ]
 
 const thanksTo = [
-  {name: "@no_side666", role: "Smart contract analyst", link: "", avatar: ""},
-  {name: "@tokenfox", role: "Smart contract analyst", link: "", avatar: ""},
+  {name: "@no_side666", role: "Smart contract analyst", link: "https://x.com/no_side666", avatar: "team-avatars/no_side666.jpg"},
+  {name: "@tokenfox", role: "Smart contract analyst", link: "https://x.com/tokenfox1", avatar: "team-avatars/tokenfox.jpg"},
 ]
 const goto = (link: string) => {
   window.open(link, "_blank")
@@ -21,26 +26,26 @@ const goto = (link: string) => {
 
 <template>
   <div class="flex flex-col gap-y-12">
-    <div class="flex flex-row items-center justify-center gap-12 flex-wrap">
+    <div class="flex flex-row items-start justify-center gap-12 flex-wrap p-6">
       <div v-for="member in teamMembers" :key="member.name"
-           class="flex flex-col items-center justify-center p-1 gap-2 flex-wrap cursor-pointer"
+           class="flex flex-col items-center justify-center p-1 gap-2 flex-wrap cursor-pointer w-[150px]"
            @click="goto(`https://x.com/${member.link}`)"
       >
-        <div class="rounded-full w-[100px] h-[100px] bg-gray-suit-700"></div>
-        <div>{{ member.name }}</div>
-        <div>{{ member.role }}</div>
+        <UAvatar size="4xl" :src="member.avatar" :alt="member.name"/>
+        <div class="font-bold text-[24px]">{{ member.name }}</div>
+        <div >{{ member.role }}</div>
       </div>
     </div>
     <div>
       <h2 class="mb-3 sir-text-shadow section-header text-rob-roy-300">A special thanks to</h2>
-      <div class="flex flex-row items-center justify-center p-1 gap-2 flex-wrap">
+      <div class="flex flex-row items-center justify-center gap-12 p-6 flex-wrap">
         <div v-for="member in thanksTo" :key="member.name"
-             class="flex flex-col items-center justify-center p-1 gap-2 flex-wrap cursor-pointer"
-             @click="goto(`https://x.com/${member.link}`)"
+             class="flex flex-col items-center justify-center p-1 gap-2 flex-wrap cursor-pointer w-[150px]"
+             @click="goto(member.link)"
         >
-          <div class="rounded-full w-[100px] h-[100px] bg-gray-suit-700"></div>
-          <div>{{ member.name }}</div>
-          <div>{{ member.role }}</div>
+          <UAvatar size="4xl" :src="member.avatar" :alt="member.name"/>
+          <div class="font-semibold text-[24px]">{{ member.name }}</div>
+          <div >{{ member.role }}</div>
         </div>
       </div>
     </div>
