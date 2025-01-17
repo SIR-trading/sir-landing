6d82bfea60082a6d5ff28a3b07857887da0c499e
abstrucked <mxhgraphics@gmail.com>
2025-01-17 15:44:34 +0100

    Refactor NavigationTail.vue for improved styling and structure
    Adjusted layout to better align items and ensure consistent spacing. Replaced `<a>` tags with `<ULink>` components for uniformity and maintainability. Added `whitespace-nowrap` to improve button text display.

--- components/layout/NavigationTail.vue
+++ components/layout/NavigationTail.vue
@@ -3,23 +3,23 @@
 </script>
 
 <template>
-  <div class="flex flex-col gap-6 md:gap-6 md:flex-row-reverse justify-around">
-    <div class="flex md:w-full justify-center">
-    <UButton color="robRoy" variant="outline" :disabled="false" :external="true"
-             to="https://prototype.sir.trading/">
-      <span class="font-sm font-semibold">Launch Prototype</span>
-    </UButton>
+  <div class="flex flex-col gap-6 md:gap-4 md:flex-row-reverse justify-around md:items-center">
+    <div class="">
+      <UButton color="robRoy" variant="outline" :disabled="false" :external="true"
+               to="https://prototype.sir.trading/">
+        <span class="font-sm font-semibold whitespace-nowrap">Launch Prototype</span>
+      </UButton>
     </div>
-    <div class="flex flex-row gap-2 sm:gap-3 md:gap-4 lg:gap-6 justify-around items-center">
-      <a href="https://x.com/leveragesir" target="_blank">
+    <div class="flex flex-row gap-2 sm:gap-3 md:gap-4 lg:gap-6 justify-around md:justify-center items-center">
+      <ULink class="flex justify-self-center" href="https://x.com/leveragesir" target="_blank">
         <UIcon name="simple-icons:x" dynamic size="20px" class="text-white"/>
-      </a>
-      <a href="https://discord.gg/M2SRBDPUR2" target="_blank">
+      </ULink>
+      <ULink class="flex justify-self-center" href="https://discord.gg/M2SRBDPUR2" target="_blank">
         <UIcon name="simple-icons:discord" dynamic size="24px" class="text-white"/>
-      </a>
-      <a href="https://github.com/SIR-trading" target="_blank">
+      </ULink>
+      <ULink class="flex justify-self-center" href="https://github.com/SIR-trading" target="_blank">
         <UIcon name="simple-icons:github" dynamic size="24px" class="text-white"/>
-      </a>
+      </ULink>
     </div>
   </div>
 </template>
