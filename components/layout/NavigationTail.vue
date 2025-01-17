f317296fcc64d5de588b1c36bad36a1e3e58a029
abstrucked <mxhgraphics@gmail.com>
2025-01-17 12:32:26 +0100

    Refactor navigation layout and simplify link structure.
    Removed unnecessary properties and templates in navigation links for cleaner structure. Adjusted layout styles for consistent spacing and improved button placement. Reintroduced "Launch Prototype" button in the navigation tail with updated layout.

--- components/layout/NavigationTail.vue
+++ components/layout/NavigationTail.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-  <div class="w-full flex flex-row gap-2 md:gap-6 justify-around">
+  <div class="flex flex-col gap-6 md:gap-6 md:flex-row-reverse justify-around">
+    <div class="flex md:w-full justify-center">
+    <UButton color="robRoy" variant="outline" :disabled="false" :external="true"
+             to="https://prototype.sir.trading/">
+      <span class="font-sm font-semibold">Launch Prototype</span>
+    </UButton>
+    </div>
     <div class="flex flex-row gap-2 sm:gap-3 md:gap-4 lg:gap-6 justify-around items-center">
       <a href="https://x.com/leveragesir" target="_blank">
         <UIcon name="simple-icons:x" dynamic size="20px" class="text-white"/>
@@ -15,10 +21,6 @@
         <UIcon name="simple-icons:github" dynamic size="24px" class="text-white"/>
       </a>
     </div>
-    <UButton color="robRoy" variant="outline" :disabled="false" :external="true"
-             to="https://prototype.sir.trading/">
-      <span class="font-sm font-semibold">Launch Prototype</span>
-    </UButton>
   </div>
 </template>
 
