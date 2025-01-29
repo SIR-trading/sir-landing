63c8f478a06346019a3d5a1fc022fb609bfa51bb
abstrucked <mxhgraphics@gmail.com>
2025-01-29 20:04:14 +0100

    Refine audit page padding and styling.
    Removed redundant padding from the container and adjusted padding for the success state element to improve layout consistency and responsiveness. No functional changes were made.

--- pages/audit.vue
+++ pages/audit.vue
@@ -3,10 +3,10 @@ const {data: pdf, status, refresh} = await useFetch<string>('/api/audit');
 </script>
 
 <template>
-  <UContainer class="flex flex-col items-center p-[1rem] gap-12 w-full justify-center screen-90-percent bg-black-russian-950 rounded-lg">
+  <UContainer class="flex flex-col items-center gap-12 w-full justify-center screen-90-percent bg-black-russian-950 rounded-lg">
     <div v-if="status === 'pending'">Loading...</div>
     <div v-else-if="status === 'error'">Error :(</div>
-    <div v-else-if="status === 'success'" class="w-full p-12 screen-90-percent">
+    <div v-else-if="status === 'success'" class="w-full px-[20px] py-[20px] screen-90-percent">
       <iframe class="rounded-lg w-full min-h-[700px] md:min-h-full" src="/SIR_Audit_Report.pdf" ></iframe>
     </div>
   </UContainer>
