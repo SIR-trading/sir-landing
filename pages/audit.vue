92b6339d8293e9c7d20f6e319b6ff2550b0de647
abstrucked <mxhgraphics@gmail.com>
2025-01-29 19:57:32 +0100

    Update navigation and audit link handling
    Renamed the audit file and updated navigation components to reflect this change. Added a direct link to the audit report in both mobile and desktop navigation layouts. Simplified and adjusted styling for improved consistency and usability.

---
+++ pages/audit.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const {data: pdf, status, refresh} = await useFetch<string>('/api/audit');
+</script>
+
+<template>
+  <UContainer class="flex flex-col items-center p-[1rem] gap-12 w-full justify-center screen-90-percent bg-black-russian-950 rounded-lg">
+    <div v-if="status === 'pending'">Loading...</div>
+    <div v-else-if="status === 'error'">Error :(</div>
+    <div v-else-if="status === 'success'" class="w-full p-12 screen-90-percent">
+      <iframe class="rounded-lg w-full min-h-[700px] md:min-h-full" src="/SIR_Audit_Report.pdf" ></iframe>
+    </div>
+  </UContainer>
+</template>
+
+<style scoped>
+.screen-90-percent {
+  height: calc(90vh - 2rem);
+}
+</style>
+
