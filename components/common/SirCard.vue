c1dd0deff64688aff9b35c08b65eda1be9782069
abstrucked <mxhgraphics@gmail.com>
2025-05-24 14:12:01 +0200

    feat: optimized for cards with image

--- components/common/SirCard.vue
+++ components/common/SirCard.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
 const { classLink } = helpers()
 const classes = computed(() => {
   return classLink([
-    'card flex flex-col justify-start rounded-lg', ` w-full md:max-w-${props.size ? `${props.size}` : 'sm'}`,
+    'card grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 rounded-lg md:gap-2', ` w-full md:max-w-${props.size ? `${props.size}` : 'sm'}`,
     props.className ? props.className : ''
   ])
 })
@@ -18,14 +18,14 @@ const classes = computed(() => {
 
 <template>
   <div :class="classes">
-    <div v-if="image">
+    <div v-if="image" class="flex justify-center">
       <img :src="image" class="rounded-lg" :alt="imageAlt" />
     </div>
-    <div class="flex flex-col flex-wrap overflow-auto">
+    <div class="flex flex-col flex-wrap overflow-auto gap-2" :class="!image ? 'md:col-span-2 lg:col-span-1' : ''">
       <h4 class="p-4 flex flex-row items-center justify-center">
         <slot name="header" />
       </h4>
-      <div class=" flex flex-col flex-wrap">
+      <div class=" flex flex-col flex-wrap lg:p-4">
         <slot name="default" />
       </div>
     </div>
